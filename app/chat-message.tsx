54c25f2e245cbac5f29464f5f4a7d08552e69cae
Jared Palmer <jared@jaredpalmer.com>
2023-05-19 14:31:21 -0400

    Move things around

--- app/chat-message.tsx
+++ app/chat-message.tsx
@@ -1,6 +1,6 @@
 import { type Message } from "@prisma/client";
-import CodeBlock from "./codeblock";
-import { MemoizedReactMarkdown } from "./markdown";
+import CodeBlock from "@/components/ui/codeblock";
+import { MemoizedReactMarkdown } from "@/components/markdown";
 import remarkGfm from "remark-gfm";
 import remarkMath from "remark-math";
 import { cn } from "@/lib/utils";
