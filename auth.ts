911829493c5d405971aa0cd9afe0da9f7f646804
Jared Palmer <jared@jaredpalmer.com>
2023-05-22 10:16:09 -0400

    Move to next-auth

---
+++ auth.ts
@@ -0,0 +1,26 @@
+import NextAuth from "@auth/nextjs";
+import GitHub from "@auth/nextjs/providers/github";
+import { NextResponse } from "next/server";
+export const {
+  handlers: { GET, POST },
+  auth,
+  CSRF_experimental,
+} = NextAuth({
+  // @ts-ignore
+  providers: [GitHub],
+  session: { strategy: "jwt" },
+  async authorized({ request, auth }: any) {
+    const url = request.nextUrl;
+
+    if (request.method === "POST") {
+      const { authToken } = (await request.json()) ?? {};
+      // If the request has a valid auth token, it is authorized
+      const valid = true;
+      if (valid) return true;
+      return NextResponse.json("Invalid auth token", { status: 401 });
+    }
+
+    // Logged in users are authenticated, otherwise redirect to login page
+    return !!auth;
+  },
+});
