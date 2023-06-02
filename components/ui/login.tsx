c0a01ef8330d9dc527ff135a523878831fa99656
Jared Palmer <jared@jaredpalmer.com>
2023-06-02 12:27:06 -0400

    Fix edge compat

--- components/ui/login.tsx
+++ components/ui/login.tsx
@@ -1,11 +1,13 @@
 "use client";
-import { signIn } from "@auth/nextjs/client";
+
+import { useRouter } from "next/navigation";
 
 export function Login() {
+  const router = useRouter();
   return (
     <button
       className="inline-flex w-full items-center justify-center rounded border border-zinc-800 bg-white h-8 px-4 -my-1.5 text-sm leading-6 tracking-tight text-zinc-900 transition-colors ease-in-out hover:bg-zinc-100 disabled:cursor-not-allowed disabled:opacity-50"
-      onClick={() => signIn("github")}
+      onClick={() => router.push("/api/auth/signin")}
     >
       <span className="font-medium">Login</span>
     </button>
