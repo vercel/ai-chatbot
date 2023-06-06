fa02c245c2fed769f7dbc112e3b31d606560d46f
Jared Palmer <jared@jaredpalmer.com>
2023-06-06 13:18:49 -0400

    Organize imports

--- components/ui/login.tsx
+++ components/ui/login.tsx
@@ -1,10 +1,8 @@
 'use client'
 
 import { signIn } from '@auth/nextjs/client'
-import { useRouter } from 'next/navigation'
 
 export function Login() {
-  const router = useRouter()
   return (
     <button
       className="inline-flex w-full items-center justify-center rounded border border-zinc-800 bg-white h-8 px-4 -my-1.5 text-sm leading-6 tracking-tight text-zinc-900 transition-colors ease-in-out hover:bg-zinc-100 disabled:cursor-not-allowed disabled:opacity-50"
