485759a31f0066c497b57645fa54840a62432bcb
shadcn <m@shadcn.com>
2023-06-13 18:14:06 +0400

    fix: update login button and sidebar

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -85,13 +85,7 @@ const SheetHeader = ({
   className,
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) => (
-  <div
-    className={cn(
-      'flex flex-col space-y-2 text-center sm:text-left',
-      className
-    )}
-    {...props}
-  />
+  <div className={cn('flex flex-col space-y-2', className)} {...props} />
 )
 SheetHeader.displayName = 'SheetHeader'
 
