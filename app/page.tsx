3fd1859005152a3ce8fa97f1438ef50728e19acf
shadcn <m@shadcn.com>
2023-06-15 16:07:03 +0400

    fix: update layouts for chat pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { nanoid } from '@/lib/utils'
 import { Chat } from '@/components/chat'
-import { Header } from '@/components/header'
 
 // export const runtime = 'edge'
 export const preferredRegion = 'home'
@@ -8,13 +7,5 @@ export const preferredRegion = 'home'
 export default function IndexPage() {
   const id = nanoid()
 
-  return (
-    <div className="flex min-h-screen flex-col">
-      {/* @ts-ignore */}
-      <Header />
-      <main className="flex-1 bg-muted/50">
-        <Chat id={id} />
-      </main>
-    </div>
-  )
+  return <Chat id={id} />
 }
