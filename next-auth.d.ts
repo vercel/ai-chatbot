fef709a21d2e14b17c81c1a2ba3929adbf546dfe
Jared Palmer <jared@jaredpalmer.com>
2023-06-16 12:03:09 -0400

    Update next-auth

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,7 +1,13 @@
+import NextAuth, { DefaultSession } from 'next-auth'
+
 declare module 'next-auth' {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
   interface Session {
     user: {
-      id: string
-    }
+      /** The user's postal address. */
+      address: string
+    } & DefaultSession['user']
   }
 }
