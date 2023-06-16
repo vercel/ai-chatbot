a57975e78bbd62588428236a6ba2004ce295493b
Jared Palmer <jared@jaredpalmer.com>
2023-06-16 16:51:39 -0400

    Fix login

--- components/login-button.tsx
+++ components/login-button.tsx
@@ -6,7 +6,7 @@ import { signIn } from 'next-auth/react'
 import { cn } from '@/lib/utils'
 import { Button, type ButtonProps } from '@/components/ui/button'
 import { IconGitHub, IconSpinner } from '@/components/ui/icons'
-import { useSearchParams } from 'next/navigation'
+
 interface LoginButtonProps extends ButtonProps {
   showGithubIcon?: boolean
   text?: string
@@ -19,18 +19,13 @@ export function LoginButton({
   ...props
 }: LoginButtonProps) {
   const [isLoading, setIsLoading] = React.useState(false)
-  const searchParams = useSearchParams()
-  const next = searchParams.get('next') || ''
 
   return (
     <Button
       variant="outline"
       onClick={() => {
         setIsLoading(true)
-        signIn('github', {
-          callbackUrl: `${window.location.origin}${next}`,
-          redirect: true
-        })
+        signIn('github')
       }}
       disabled={isLoading}
       className={cn(className)}
