9d63e761a8774db3bc8dc5656bae33fa9ddb265e
Jared Palmer <jared@jaredpalmer.com>
2023-06-21 14:01:05 -0700

    Only ask for tokens on preview

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -18,7 +18,7 @@ export async function POST(req: Request) {
   const { messages, previewToken } = json
   const session = await auth()
 
-  if (process.env.VERCEL_ENV !== 'preview') {
+  if (process.env.VERCEL_ENV === 'preview') {
     if (session == null) {
       return new Response('Unauthorized', { status: 401 })
     }
