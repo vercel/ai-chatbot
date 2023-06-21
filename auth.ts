4b7d590bf10193d3084fc93b54945ecc4db5e0d9
Jared Palmer <jared@jaredpalmer.com>
2023-06-21 14:32:54 -0700

    Revert "Merge pull request #74 from vercel-labs:jp/next-auth"
    This reverts commit 6051e186b5c3820eb35b82bcb53a6c0e8498c25b, reversing
    changes made to 28f62d80cf90cb24beb837932c308f3a14bb5230.

--- auth.ts
+++ auth.ts
@@ -1,26 +1,16 @@
-import NextAuth, { type DefaultSession } from 'next-auth'
+import NextAuth from 'next-auth'
 import GitHub from 'next-auth/providers/github'
-
-declare module 'next-auth' {
-  /**
-   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
-   */
-  interface Session {
-    user: {
-      /** The user's postal address. */
-      id: string
-    } & DefaultSession['user']
-  }
-}
 
 export const {
   handlers: { GET, POST },
   auth,
   CSRF_experimental
+  // @ts-ignore
 } = NextAuth({
   // @ts-ignore
   providers: [GitHub],
   callbacks: {
+    // @ts-ignore
     jwt: async ({ token, profile }) => {
       if (profile?.id) {
         token.id = profile.id
