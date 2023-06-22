74362693cab45a2b099246f013350f758d471f4d
Jared Palmer <jared@jaredpalmer.com>
2023-06-22 10:54:54 -0700

    Fix og image

--- app/share/[id]/opengraph-image.tsx
+++ app/share/[id]/opengraph-image.tsx
@@ -93,9 +93,7 @@ export default async function Image({ params }: ImageProps) {
               <div tw="flex text-[#eaeaf0] ml-2">KV</div>
             </div>
           </div>
-          <div tw="text-[1.8rem] ml-auto text-[#9b9ba4]">
-            demo.chat.vercel.ai
-          </div>
+          <div tw="text-[1.8rem] ml-auto text-[#9b9ba4]">chat.vercel.ai</div>
         </div>
       </div>
     ),
