a1700a86c329b7b4d138b9eb04e61c9c80258ef3
Doma <leishenghao@126.com>
2023-08-31 10:42:49 +0800

    Cleanup setTimeout when useCopyToClipboard hook unmounts

--- lib/hooks/use-copy-to-clipboard.tsx
+++ lib/hooks/use-copy-to-clipboard.tsx
@@ -11,6 +11,16 @@ export function useCopyToClipboard({
 }: useCopyToClipboardProps) {
   const [isCopied, setIsCopied] = React.useState<Boolean>(false)
 
+  const timeoutRef = React.useRef<ReturnType<typeof setTimeout>>();
+  
+  React.useEffect(function cleanupTimeoutOnUnmount() {
+    return () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current)
+      }
+    }
+  }, [])
+
   const copyToClipboard = (value: string) => {
     if (typeof window === 'undefined' || !navigator.clipboard?.writeText) {
       return
@@ -23,7 +33,10 @@ export function useCopyToClipboard({
     navigator.clipboard.writeText(value).then(() => {
       setIsCopied(true)
 
-      setTimeout(() => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current)
+      }
+      timeoutRef.current = setTimeout(() => {
         setIsCopied(false)
       }, timeout)
     })
