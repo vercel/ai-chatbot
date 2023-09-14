e2ad7df34a42e95b97a3fbe32013c87cbf3bdf12
Markeljan Sokoli <Markeljan19@gmail.com>
2023-09-14 17:29:59 -0400

    Update use-local-storage.ts
    Throws ReferenceError: window is not defined server-side
    
    Seems to be the best alternative v.s. having to use isClient & having hydration errors.

--- lib/hooks/use-local-storage.ts
+++ lib/hooks/use-local-storage.ts
@@ -1,25 +1,20 @@
-import { useEffect, useState } from 'react'
+import { useState, useEffect } from 'react'
 
 export const useLocalStorage = <T>(
   key: string,
   initialValue: T
 ): [T, (value: T) => void] => {
   const [storedValue, setStoredValue] = useState<T>(() => {
-    if (typeof window !== 'undefined') {
-      const item = window.localStorage.getItem(key)
-      return item ? JSON.parse(item) : initialValue
-    }
-    return initialValue
+    // causes ReferenceError: window is not defined but avoids hydration error
+    const item = window.localStorage.getItem(key)
+    return item ? JSON.parse(item) : initialValue
   })
 
   useEffect(() => {
-    if (typeof window !== 'undefined') {
-      window.localStorage.setItem(key, JSON.stringify(storedValue))
-    }
+    window.localStorage.setItem(key, JSON.stringify(storedValue))
   }, [storedValue, key])
 
   const setValue = (value: T) => {
-    // Save state
     setStoredValue(value)
   }
   return [storedValue, setValue]
