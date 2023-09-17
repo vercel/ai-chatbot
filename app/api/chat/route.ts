660bff8b11833d87e3cebcd3cdc1745d0e066dca
QuantumSlipstream <tgif.lv@gmail.com>
2023-09-17 12:23:14 +0100

    feat: Introduce OPENAI_MODEL env variable support

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -30,7 +30,7 @@ export async function POST(req: Request) {
   }
 
   const res = await openai.createChatCompletion({
-    model: 'gpt-3.5-turbo',
+    model: process.env.OPENAI_MODEL ?? 'gpt-3.5-turbo',
     messages,
     temperature: 0.7,
     stream: true
