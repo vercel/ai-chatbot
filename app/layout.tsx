f261a0eebeafa6132eb8e8f3ee17ee4c145d25fd
Lee Robinson <lrobinson2011@gmail.com>
2023-11-26 12:07:13 -0600

    Start updating deps

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,17 @@
 import { Metadata } from 'next'
 
 import { Toaster } from 'react-hot-toast'
+import { GeistSans } from 'geist/font/sans'
+import { GeistMono } from 'geist/font/mono'
 
 import '@/app/globals.css'
-import { fontMono, fontSans } from '@/lib/fonts'
 import { cn } from '@/lib/utils'
 import { TailwindIndicator } from '@/components/tailwind-indicator'
 import { Providers } from '@/components/providers'
 import { Header } from '@/components/header'
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://chat.vercel.ai/'),
   title: {
     default: 'Next.js AI Chatbot',
     template: `%s - Next.js AI Chatbot`
@@ -33,18 +35,16 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head />
       <body
         className={cn(
           'font-sans antialiased',
-          fontSans.variable,
-          fontMono.variable
+          GeistSans.variable,
+          GeistMono.variable
         )}
       >
         <Toaster />
         <Providers attribute="class" defaultTheme="system" enableSystem>
           <div className="flex flex-col min-h-screen">
-            {/* @ts-ignore */}
             <Header />
             <main className="flex flex-col flex-1 bg-muted/50">{children}</main>
           </div>
