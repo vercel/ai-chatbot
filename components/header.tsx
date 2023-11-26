3e952b867fe3024cd12c5440d4abf75b58f35836
Lee Robinson <lrobinson2011@gmail.com>
2023-11-26 12:07:13 -0600

    Start updating deps

--- components/header.tsx
+++ components/header.tsx
@@ -17,7 +17,6 @@ import { SidebarFooter } from '@/components/sidebar-footer'
 import { ThemeToggle } from '@/components/theme-toggle'
 import { ClearHistory } from '@/components/clear-history'
 import { UserMenu } from '@/components/user-menu'
-import { LoginButton } from '@/components/login-button'
 
 export async function Header() {
   const session = await auth()
