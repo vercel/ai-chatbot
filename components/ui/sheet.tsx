7bfa34dc559b1fd7b1a4dc4c4f5dabcd9495a2d5
Lee Robinson <lrobinson2011@gmail.com>
2023-11-26 19:29:26 -0600

    Moar

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -56,7 +56,7 @@ interface SheetContentProps
 const SheetContent = React.forwardRef<
   React.ElementRef<typeof SheetPrimitive.Content>,
   SheetContentProps
->(({ side = 'right', className, children, ...props }, ref) => (
+>(({ side = 'left', className, children, ...props }, ref) => (
   <SheetPortal>
     <SheetOverlay />
     <SheetPrimitive.Content
