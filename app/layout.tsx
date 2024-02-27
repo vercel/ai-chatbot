27a3b290ebd9cd2926336f0e60488e6c5f0cb7f8
dadsec-dev <geecypher41@gmail.com>
2024-02-27 12:55:01 +0100

    typo

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,14 +11,14 @@ import { Header } from '@/components/header'
 export const metadata = {
   metadataBase: new URL(`https://${process.env.VERCEL_URL}`),
   title: {
-    default: 'Next.js AI Chatbot',
-    template: `%s - Next.js AI Chatbot`
+    default: 'OCADA AI',
+    template: `%s - OCADA AI`
   },
-  description: 'An AI-powered chatbot template built with Next.js and Vercel.',
+  description: 'The first AI Agent in web3',
   icons: {
-    icon: '/favicon.ico',
-    shortcut: '/favicon-16x16.png',
-    apple: '/apple-touch-icon.png'
+    icon: '/OCADA.svg',
+    shortcut: '/OCADA.svg',
+    apple: '/OCADA.svg'
   }
 }
 
