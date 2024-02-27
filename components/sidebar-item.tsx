783e1ac739d965327dfb6af4f8404e9d4d38fe56
lo_mikeal <mikealnamdy@gmail.com>
2024-02-27 12:24:42 +0100

    fix sidebar ui

--- components/sidebar-item.tsx
+++ components/sidebar-item.tsx
@@ -73,7 +73,7 @@ export function SidebarItem({ index, chat, children }: SidebarItemProps) {
         className={cn(
           buttonVariants({ variant: 'ghost' }),
           'group w-full px-8 transition-colors hover:bg-zinc-200/40 dark:hover:bg-zinc-300/10',
-          isActive && 'bg-zinc-200 pr-16 font-semibold dark:bg-zinc-800'
+          isActive && 'bg-transparent pr-16 font-semibold dark:bg-transparent'
         )}
       >
         <div
