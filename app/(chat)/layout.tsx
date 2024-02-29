3681f03f31fea8a6e70d443aeef4d2c1d3b52dd9
lo_mikeal <mikealnamdy@gmail.com>
2024-02-29 16:12:19 +0100

    fix right bar gap

--- app/(chat)/layout.tsx
+++ app/(chat)/layout.tsx
@@ -10,7 +10,7 @@ export default async function ChatLayout({ children }: ChatLayoutProps) {
     <section className="relative flex h-screen overflow-hidden">
       <SidebarDesktop />
       <div className="group w-full overflow-auto pl-0 animate-in duration-300 ease-in-out peer-[[data-state=open]]:lg:ml-[220px] peer-[[data-state=open]]:xl:ml-[220px]">
-        <article className="md:grid grid-cols-16 gap-4 mx-auto">
+        <article className="md:grid grid-cols-16 gap-1 mx-auto">
           <main className="col-start-1 col-end-12 relative">{children}</main>
           <aside className="col-span-5 col-start-12 overflow-y-scroll">
             <Component />
