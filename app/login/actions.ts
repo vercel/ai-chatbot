15a7b0a4ad8af473ded73d2a19420d805eb95169
jeremyphilemon <git@jrmyphlmn.com>
2024-03-08 16:50:15 +0300

    Add salt prefix for passwords

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -12,7 +12,15 @@ export async function authenticate(
   try {
     const email = formData.get('email')
     const password = formData.get('password')
-    await signIn('credentials', { email, password, redirectTo: '/' })
+
+    const saltPrefix = process.env.AUTH_SALT_PREFIX || ''
+    const passwordWithPrefix = saltPrefix + password
+
+    await signIn('credentials', {
+      email,
+      password: passwordWithPrefix,
+      redirectTo: '/'
+    })
   } catch (error) {
     if (error instanceof AuthError) {
       switch (error.type) {
@@ -33,27 +41,38 @@ export async function signup(
 ) {
   const email = formData.get('email') as string
   const password = formData.get('password') as string
-  const hashedPassword = await bcrypt.hash(password, 10)
 
-  const client = await db.connect()
+  if (email.length >= 6 && password.length >= 6) {
+    const saltPrefix = process.env.AUTH_SALT_PREFIX || ''
+    const saltRounds = 10
+    const salt = await bcrypt.genSalt(saltRounds)
+    const passwordWithPrefix = saltPrefix + password
+    const hashedPassword = await bcrypt.hash(passwordWithPrefix, salt)
 
-  try {
-    await client.sql`
-        INSERT INTO users (email, password)
-        VALUES (${email}, ${hashedPassword})
-        ON CONFLICT (id) DO NOTHING;
-      `
+    const client = await db.connect()
 
-    return 'User created! Please log in.'
-  } catch (error) {
-    const { message } = error as Error
+    try {
+      await client.sql`
+          INSERT INTO users (email, password)
+          VALUES (${email}, ${hashedPassword})
+          ON CONFLICT (id) DO NOTHING;
+        `
 
-    if (message.startsWith('duplicate key value violates unique constraint')) {
-      return 'User already exists! Please log in.'
-    } else {
-      return 'Something went wrong! Please try again.'
+      return 'User created! Please log in.'
+    } catch (error) {
+      const { message } = error as Error
+
+      if (
+        message.startsWith('duplicate key value violates unique constraint')
+      ) {
+        return 'User already exists! Please log in.'
+      } else {
+        return 'Something went wrong! Please try again.'
+      }
+    } finally {
+      client.release()
     }
-  } finally {
-    client.release()
+  } else {
+    return 'Email and password must be at least 6 characters long.'
   }
 }
