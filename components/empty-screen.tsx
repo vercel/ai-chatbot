050feebd475b3022d422c6c7130b8551531733e2
Srujan Gurram <52039218+Royal-lobster@users.noreply.github.com>
2024-04-14 20:02:17 +0530

    Removes unused example messages array

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -4,21 +4,6 @@ import { Button } from '@/components/ui/button'
 import { ExternalLink } from '@/components/external-link'
 import { IconArrowRight } from '@/components/ui/icons'
 
-const exampleMessages = [
-  {
-    heading: 'Explain technical concepts',
-    message: `What is a "serverless function"?`
-  },
-  {
-    heading: 'Summarize an article',
-    message: 'Summarize the following article for a 2nd grader: \n'
-  },
-  {
-    heading: 'Draft an email',
-    message: `Draft an email to my boss about the following: \n`
-  }
-]
-
 export function EmptyScreen() {
   return (
     <div className="mx-auto max-w-2xl px-4">
