01fb1c2a0e0c779505fd35c8c41147018826556f
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2024-05-10 13:27:18 -0700

    removed erroneous component

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 import { cn } from '@/lib/utils'
-import { ExternalLink } from '@/components/external-link'
 
 export function FooterText({ className, ...props }: React.ComponentProps<'p'>) {
   return (
