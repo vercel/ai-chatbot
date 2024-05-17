667ea2c3076a739e8f6278ec958e61584a33aa6b
Grace Yun <grace.yun@vercel.com>
2024-05-17 16:51:06 -0400

    lucide react icons

--- components/events/date-picker.tsx
+++ components/events/date-picker.tsx
@@ -2,7 +2,7 @@
 
 import * as React from 'react'
 import { format } from 'date-fns'
-// import { Calendar as CalendarIcon } from 'lucide-react'
+import { Calendar as CalendarIcon } from 'lucide-react'
 import { cn } from '@/lib/utils'
 import { Button } from '@/components/ui/button'
 import { Calendar } from '@/components/ui/calendar'
@@ -29,7 +29,7 @@ export function DatePicker({
             !date && 'text-muted-foreground'
           )}
         >
-          {/* <CalendarIcon className="mr-2 size-4" /> */}
+          <CalendarIcon className="mr-2 size-4" />
           {date ? format(date, 'PPP') : <span>Pick a date</span>}
         </Button>
       </PopoverTrigger>
