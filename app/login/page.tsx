1d626f5688a7b1da4b8adba475c1cea4c6100e32
Sujith S <sujithsuresh246@gmail.com>
2024-05-28 21:59:23 +0530

    fix: remove lock file

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,3 @@
-import { auth } from '@/auth'
-import LoginForm from '@/components/login-form'
-import { Session } from '@/lib/types'
-import { redirect } from 'next/navigation'
 import Login from '@/components/login'
 
 export default async function LoginPage() {
