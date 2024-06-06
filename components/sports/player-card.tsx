c8ab2c1bd2cb113a5017e5352651b477997731c2
Ben <ben@fantasyfootballdatapros.com>
2024-06-06 09:45:24 -0400

    refactored code for caching stuff

--- components/sports/player-card.tsx
+++ components/sports/player-card.tsx
@@ -1,34 +1,27 @@
 import React from 'react';
+import { Player } from '@/lib/types';
+
 
 export interface PlayerCardProps {
-    playerInfo: {
-        player_name: string;
-        team: string;
-        position: string;
-        height: string;
-        weight: string;
-        college: string;
-        headshot_url: string;
-    };
+    player: Player;
 }
 
-export function PlayerCard({ playerInfo }: PlayerCardProps) {
-    console.log('player info: ', playerInfo)
+export function PlayerCard({ player }: PlayerCardProps) {
     return (
         <div
             className="p-4 mb-4 bg-white w-full transition-shadow duration-300 flex flex-row"
         >
             <img 
                 className="w-1/4 h-auto object-cover rounded-md border mr-4" 
-                src={playerInfo.headshot_url} 
-                alt={`${playerInfo.player_name}`} />
+                src={player.image} 
+                alt={`${player.playerName}`} />
             <div className="p-2 w-3/4">
-                <h3 className="text-xl font-bold text-gray-800 mb-2">{playerInfo.player_name}</h3>
-                <p className="text-md text-gray-600 mb-1">Team: {playerInfo.team}</p>
-                <p className="text-md text-gray-600 mb-1">Position: { playerInfo.position }</p>
-                <p className="text-md text-gray-600 mb-1">Height: {playerInfo.height}</p>
-                <p className="text-md text-gray-600 mb-1">Weight: {playerInfo.weight}</p>
-                <p className="text-md text-gray-600 mb-1">College: {playerInfo.college}</p>
+                <h3 className="text-xl font-bold text-gray-800 mb-2">{player.playerName}</h3>
+                <p className="text-md text-gray-600 mb-1">Team: {player.team}</p>
+                <p className="text-md text-gray-600 mb-1">Position: { player.position }</p>
+                <p className="text-md text-gray-600 mb-1">Height: {player.height}</p>
+                <p className="text-md text-gray-600 mb-1">Weight: {player.weight}</p>
+                <p className="text-md text-gray-600 mb-1">College: {player.college}</p>
             </div>
         </div>
     );
