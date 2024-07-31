561d01e7682e6e27f3206593085fa768d276d9c2
jeremyphilemon <git@jrmyphlmn.com>
2024-07-31 19:43:54 +0530

    Fix types

--- components/sidebar-list.tsx
+++ components/sidebar-list.tsx
@@ -2,6 +2,7 @@ import { clearChats, getChats } from '@/app/actions'
 import { ClearHistory } from '@/components/clear-history'
 import { SidebarItems } from '@/components/sidebar-items'
 import { ThemeToggle } from '@/components/theme-toggle'
+import { redirect } from 'next/navigation'
 import { cache } from 'react'
 
 interface SidebarListProps {
@@ -16,23 +17,27 @@ const loadChats = cache(async (userId?: string) => {
 export async function SidebarList({ userId }: SidebarListProps) {
   const chats = await loadChats(userId)
 
-  return (
-    <div className="flex flex-1 flex-col overflow-hidden">
-      <div className="flex-1 overflow-auto">
-        {chats?.length ? (
-          <div className="space-y-2 px-2">
-            <SidebarItems chats={chats} />
-          </div>
-        ) : (
-          <div className="p-8 text-center">
-            <p className="text-sm text-muted-foreground">No chat history</p>
-          </div>
-        )}
+  if (!chats || 'error' in chats) {
+    redirect('/')
+  } else {
+    return (
+      <div className="flex flex-1 flex-col overflow-hidden">
+        <div className="flex-1 overflow-auto">
+          {chats?.length ? (
+            <div className="space-y-2 px-2">
+              <SidebarItems chats={chats} />
+            </div>
+          ) : (
+            <div className="p-8 text-center">
+              <p className="text-sm text-muted-foreground">No chat history</p>
+            </div>
+          )}
+        </div>
+        <div className="flex items-center justify-between p-4">
+          <ThemeToggle />
+          <ClearHistory clearChats={clearChats} isEnabled={chats?.length > 0} />
+        </div>
       </div>
-      <div className="flex items-center justify-between p-4">
-        <ThemeToggle />
-        <ClearHistory clearChats={clearChats} isEnabled={chats?.length > 0} />
-      </div>
-    </div>
-  )
+    )
+  }
 }
