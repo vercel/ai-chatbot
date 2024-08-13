086076632327b802cca6688475380439849881ae
Esteban Dalel R <esteban@astronuts.io>
2024-08-14 00:22:49 +0200

    Reduce logging

--- middleware.ts
+++ middleware.ts
@@ -14,6 +14,6 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
-    '/((?!api|_next/static|_next/image|favicon.ico).*)'
+    '/((?!api|_next/static|_next/image|/__nextjs_original-stack-frame|favicon.ico).*)'
   ]
 }
