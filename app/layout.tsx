cc426f236d0da22c8b496b41235848f3d12a5d8d
Esteban Dalel R <esteban@astronuts.io>
2024-08-16 21:26:33 +0200

    Fix metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,10 +14,10 @@ export const metadata = {
     ? new URL(`https://${process.env.VERCEL_URL}`)
     : undefined,
   title: {
-    default: 'Next.js AI Chatbot',
-    template: `%s - Next.js AI Chatbot`
+    default: 'Edgen AI Chat',
+    template: `%s - Edgen AI Chat`
   },
-  description: 'An AI-powered chatbot template built with Next.js and Vercel.',
+  description: 'An AI-powered English teaching chatbot',
   icons: {
     icon: '/favicon.ico',
     shortcut: '/favicon-16x16.png',
