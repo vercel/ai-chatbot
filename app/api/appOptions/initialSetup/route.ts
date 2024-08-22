2840e04418395c88594398fa48cfba01c1639d4b
Esteban Dalel R <esteban@astronuts.io>
2024-08-22 16:11:08 +0200

    Post to db if data comes

--- app/api/appOptions/initialSetup/route.ts
+++ app/api/appOptions/initialSetup/route.ts
@@ -2,7 +2,19 @@ import supabase from '@/lib/supabase/supabase'
 
 export async function POST(request: Request) {
   const res = await request.json()
-  console.log(res)
+  const { payment, onboarding_complete, user_id } = res
+  if (payment || onboarding_complete) {
+    const { data, error } = await supabase
+      .from('classes')
+      .upsert({
+        payment,
+        onboarding_complete
+      })
+      .eq('user', user_id)
+    if (error) throw error
+
+    return Response.json({ data })
+  }
   const data = {
     payment: false,
     onboarding_complete: false
