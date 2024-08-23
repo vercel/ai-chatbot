33693099418a8e45788b728556a06b7f820864fb
Esteban Dalel R <esteban@astronuts.io>
2024-08-23 15:30:31 +0200

    Add user_data route

---
+++ app/api/app_options/user_data/route.ts
@@ -0,0 +1,24 @@
+import supabase from '@/lib/supabase/supabase'
+
+export async function POST(request: Request) {
+  const res = await request.json()
+  const { payment, onboarding_complete, user_id } = res
+  if (!user_id) {
+    return
+  }
+  const { data, error } = await supabase
+    .from('users')
+    .select()
+    .eq('firebase_id', user_id)
+  if (error) throw error
+
+  return Response.json({ data })
+}
+export async function GET(request: Request) {
+  const data = {
+    payment: false,
+    onboarding_complete: false
+  }
+
+  return Response.json({ data })
+}
