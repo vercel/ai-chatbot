2f5642d22c110f181171cb6cdd8efaf56f148a8f
Esteban Dalel R <esteban@astronuts.io>
2024-08-27 19:55:53 +0200

    Update route.ts

--- app/api/user/preferences/route.ts
+++ app/api/user/preferences/route.ts
@@ -1,9 +1,22 @@
+import check_missing_fields from '@/lib/api/check_missing_fields'
 import supabase from '@/lib/supabase/supabase'
 
 export async function POST(request: Request) {
   const res = await request.json()
-  console.log(res)
-  const { data, error } = await supabase.from('classes').select()
+  const missing_fields = check_missing_fields({
+    fields: ['user_id', 'preferences'],
+    reqBody: res
+  })
+  if (missing_fields) {
+    return Response.json({ missing_fields })
+  }
+  const preferencesToUpsert = res.preferences.map((preference: string) => ({
+    user: res.user_id,
+    preference: preference
+  }))
+  const { data, error } = await supabase
+    .from('preferences')
+    .upsert(preferencesToUpsert)
   if (error) throw error
 
   return Response.json({ data })
