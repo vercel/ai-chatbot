09a1c76364e03dbae64692fd0f9dc7ff595948b5
fabio <fabio@kali.168.1.9>
2024-09-01 16:16:45 -0500

    Configuraciones groq api key

--- lib/chat/actions.tsx
+++ lib/chat/actions.tsx
@@ -8,7 +8,7 @@ import {
   streamUI,
   createStreamableValue
 } from 'ai/rsc'
-import { openai } from '@ai-sdk/openai'
+import { createOpenAI} from '@ai-sdk/openai'
 
 import {
   spinner,
@@ -36,6 +36,11 @@ import { SpinnerMessage, UserMessage } from '@/components/stocks/message'
 import { Chat, Message } from '@/lib/types'
 import { auth } from '@/auth'
 
+
+const groq = createOpenAI({
+  baseURL: 'https://api.groq.com/openai/v1',
+  apiKey: process.env.GROQ_API_KEY,
+});
 async function confirmPurchase(symbol: string, price: number, amount: number) {
   'use server'
 
@@ -127,7 +132,7 @@ async function submitUserMessage(content: string) {
   let textNode: undefined | React.ReactNode
 
   const result = await streamUI({
-    model: openai('gpt-3.5-turbo'),
+    model: groq('mixtral-8x7b-32768'),
     initial: <SpinnerMessage />,
     system: `\
     You are a stock trading conversation bot and you can help users buy stocks, step by step.
