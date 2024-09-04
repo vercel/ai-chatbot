43d5778e3209a3dae890343f422b7e048b892ffc
Esteban Dalel R <esteban@astronuts.io>
2024-09-04 15:13:18 +0200

    Add speaking goals

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -16,17 +16,23 @@ export async function POST(req: Request) {
     The student is the user and will try to communicate english. 
     Be patient and correct them nicely. You will try to only respond in  English. If the student is adamant on a translation, give in, but try your best to explain the problem words in English before. The student will ask you questions and you will answer them. 
     You must keep the conversation going by asking a followup.
+    You might be given a speaking goal, a performance guide and a vocabulary list, which are all important to keep in mind.
     Try to guide the student towards the target vocabulary, but don't force it.
     `
   const classTypes: { [key: string]: string } = {
     free: '',
-    restaurant: `Act as a waiter at a restaurant that specializes in several international cuisines. Ask the customer if they have any questions about the menu, explain how the dishes are prepared, and offer recommendations based on their tastes.
+    restaurant: `Speaking goal: Ask about menu items and recommend international cuisine.
+    Act as a waiter at a restaurant that specializes in several international cuisines. Ask the customer if they have any questions about the menu, explain how the dishes are prepared, and offer recommendations based on their tastes.
     The target vocabulary is: eaten, specialty, originates, grilled, stuffed, fresh, recommendation.`,
-    tourist: `Act as someone who has never visited the city where your friend lives. Try to plan a trip to visit them, asking for advice about what to see and do.
+    tourist: `Speaking goal: Suggest interesting places to go in your city.
+    Act as someone who has never visited the city where your friend lives. Try to plan a trip to visit them, asking for advice about what to see and do.
     The target vocabulary is: sight, tourist attraction, famous, statue, national park, must-see, suggestion, cuisine, depends.`,
-    invitations: `Act as a friendly colleague. If your coworker invites you to an event, ask for relevant further information and determine if you will be able to attend. If you can, then confirm the details and express excitement . If you cannot, then politely decline and express your regret.											
+    invitations: `Speaking goal: Extend invitations. 
+    Act as a friendly colleague. If your coworker invites you to an event, ask for relevant further information and determine if you will be able to attend. If you can, then confirm the details and express excitement . If you cannot, then politely decline and express your regret.											
     The target vocabulary is: annual, attend, formal, informal, luncheon, invite, Would you like to.`,
-    occasion: `Act as someone who is curious about their neighbor's upcoming plans for a special occasion. Ask what the occasion is and what they are doing to celebrate. Be friendly and ask relevant follow-up questions.
+    occasion: `
+    Speaking goal: Talk about an upcoming special occasion. 
+    Act as someone who is curious about their neighbor's upcoming plans for a special occasion. Ask what the occasion is and what they are doing to celebrate. Be friendly and ask relevant follow-up questions.
     The target vocabulary is: party, have, special occasion, graduation, housewarming, baby shower, hold.`
   }
   const result = await streamText({
