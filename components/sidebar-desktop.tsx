9afd74486b180df704ceb29f79c5f114d7a6a2a0
Esteban Dalel R <esteban@astronuts.io>
2024-09-05 11:36:00 +0200

    Remove unnnecessary code

--- components/sidebar-desktop.tsx
+++ components/sidebar-desktop.tsx
@@ -12,8 +12,7 @@ export async function SidebarDesktop() {
 
   return (
     <Sidebar className="peer absolute inset-y-0 z-30 hidden -translate-x-full border-r bg-muted duration-300 ease-in-out data-[state=open]:translate-x-0 lg:flex lg:w-[250px] xl:w-[300px]">
-      {/* @ts-ignore */}
-      <ChatHistory userId={session.user.id} />
+      <ChatHistory />
     </Sidebar>
   )
 }
