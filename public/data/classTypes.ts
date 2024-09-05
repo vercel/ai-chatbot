9699397f753c5e91cd5b4bb2ad636c822fa8ab3e
Esteban Dalel R <esteban@astronuts.io>
2024-09-05 13:06:08 +0200

    Add id

--- public/data/classTypes.ts
+++ public/data/classTypes.ts
@@ -1,9 +1,11 @@
 const classTypes: {
+  id: string
   name: string
   vocabulary: string[]
   description: string
 }[] = [
   {
+    id: '1',
     name: 'Identify important people and places in a hospital',
     vocabulary: [
       'medical',
@@ -16,6 +18,7 @@ const classTypes: {
     description: 'Lesson #1: Identify important people and places in a hospital'
   },
   {
+    id: '2',
     name: 'Describe safety procedures',
     vocabulary: [
       'gloves',
@@ -28,6 +31,7 @@ const classTypes: {
     description: 'Lesson #2: Describe safety procedures'
   },
   {
+    id: '3',
     name: 'Communicate a patient’s vital signs with the medical team',
     vocabulary: [
       'weight',
@@ -41,6 +45,7 @@ const classTypes: {
       'Lesson #3: Communicate a patient’s vital signs with the medical team'
   },
   {
+    id: '4',
     name: 'Ask about a patient’s medical history',
     vocabulary: [
       'medical history',
@@ -53,6 +58,7 @@ const classTypes: {
     description: 'Lesson #4: Ask about a patient’s medical history'
   },
   {
+    id: '5',
     name: 'Talk to a patient after an accident',
     vocabulary: ['hurt', 'pain', 'bone', 'fracture', 'sprain', 'treatment'],
     description: 'Lesson #5: Talk to a patient after an accident'
