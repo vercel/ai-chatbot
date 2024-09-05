f9562d293e93719bbe750ca8bbbea381640ad651
Esteban Dalel R <esteban@astronuts.io>
2024-09-05 12:27:35 +0200

    Create backgrounds.ts

---
+++ public/data/backgrounds.ts
@@ -0,0 +1,23 @@
+const Backgrounds = [
+  {
+    name: 'Restaurant',
+    src: '/bg0.jpg'
+  },
+  {
+    name: 'White',
+    src: '/bg1.jpg'
+  },
+  {
+    name: 'Street',
+    src: '/bg2.jpg'
+  },
+  {
+    name: 'House',
+    src: '/bg3.jpg'
+  },
+  {
+    name: 'Office',
+    src: '/bg4.jpg'
+  }
+]
+export default Backgrounds
