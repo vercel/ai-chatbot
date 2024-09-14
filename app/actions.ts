a9e2761199e30f5a6aae1367af85c456a6a9abef
Yiyun Yao <106117469+nyonyoko@users.noreply.github.com>
2024-09-14 18:39:01 -0400

    Fixing key requiring TUNE

--- app/actions.ts
+++ app/actions.ts
@@ -165,7 +165,7 @@ export async function refreshHistory(path: string) {
 }
 
 export async function getMissingKeys() {
-  const keysRequired = ['OPENAI_API_KEY']
+  const keysRequired = ['TUNE_STUDIO_API_KEY']
   return keysRequired
     .map(key => (process.env[key] ? '' : key))
     .filter(key => key !== '')
