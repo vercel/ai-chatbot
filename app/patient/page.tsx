3f10e60437437dec0d81601eaa6670330d71ff73
faith.villarr <villa.faith21@gmail.com>
2024-09-15 02:35:43 -0400

    Added HTML, Styles and Layouts

--- app/patient/page.tsx
+++ app/patient/page.tsx
@@ -12,19 +12,53 @@ import { ProfileForm } from './components/form'
 
 export default function Page() {
   return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button variant="outline">Edit Profile</Button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
-          <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
-          </DialogDescription>
-        </DialogHeader>
-        <ProfileForm />
-      </DialogContent>
-    </Dialog>
+    <>
+  <div className="main-container flex column" style={{}}>
+    <div className="heading flex" id="name-heading" style={{ fontWeight: 900 }}>
+      Welcome back,.
+    </div>
+    <div className="flex">
+      <div className="flex column" style={{ flexBasis: "60%" }}>
+        <div className="box-section" id="graph" style={{ height: "40vh" }}>
+          <h1>Your Sleep Trends</h1>
+        </div>
+        <div className="flex">
+          <div className="box-section fifty" id="upcoming">
+            <h2>Upcoming Appointments</h2>
+          </div>
+          <div className="box-section fifty" id="alerts">
+            <h2>Medical Alerts</h2>
+          </div>
+        </div>
+      </div>
+      <div className="flex column flex-grow">
+        <div
+          className="box-section flex clickable"
+          style={{ minHeight: "30vh" }}
+        >
+          <div style={{ alignContent: "center" }}>
+            <h1>Chat with Teddy!</h1>
+            <h3>Feeling under the weather? I can help!</h3>
+          </div>
+          <img
+            src="teddy.png"
+            alt="A cute teddy bear."
+            style={{
+              maxWidth: "50%",
+              maxHeight: "100%",
+              flexBasis: "35%",
+              alignSelf: "center"
+            }}
+          />
+        </div>
+        <div className="box-section flex-grow">
+          <h1>Health Tips</h1>
+          <div id="health-tips" />
+        </div>
+      </div>
+    </div>
+  </div>
+</>
+
   )
 }
