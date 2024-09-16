54d7dad03d383c929b931ab2dbd9c43017e07cb3
Julien Goux <hi@jgoux.dev>
2024-09-16 16:01:20 +0200

    remove unused code

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -1,15 +1,8 @@
 'use server'
 
-import { User } from '@/lib/types'
 import { z } from 'zod'
-import { kv } from '@vercel/kv'
 import { ResultCode } from '@/lib/utils'
 import { createClient } from '@/utils/supabase/server'
-
-export async function getUser(email: string) {
-  const user = await kv.hgetall<User>(`user:${email}`)
-  return user
-}
 
 interface Result {
   type: string
