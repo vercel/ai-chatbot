7a50ba9a9c52dc00a76c97d7b033df4e04995f35
Mahir Kalra <mahir.kalra@gmail.com>
2024-09-19 18:07:14 -0400

    nit fix

--- components/ui/codeblock.tsx
+++ components/ui/codeblock.tsx
@@ -7,9 +7,9 @@ import { FC, memo } from 'react'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { coldarkDark } from 'react-syntax-highlighter/dist/cjs/styles/prism'
 
-import { useCopyToClipboard } from '@/lib/hooks/use-copy-to-clipboard'
-import { IconCheck, IconCopy, IconDownload } from '@/components/ui/icons'
 import { Button } from '@/components/ui/button'
+import { IconCheck, IconCopy, IconDownload } from '@/components/ui/icons'
+import { useCopyToClipboard } from '@/lib/hooks/use-copy-to-clipboard'
 
 interface Props {
   language: string
