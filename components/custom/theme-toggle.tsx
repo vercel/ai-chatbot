ac2659255e7d2b1935410132510400c66da57510
Jared Palmer <jared@jaredpalmer.com>
2024-10-23 12:21:30 -0400

    Upgrade to Next.js 15.0.1 (#454)

--- components/custom/theme-toggle.tsx
+++ components/custom/theme-toggle.tsx
@@ -1,28 +1,28 @@
-"use client";
+'use client';
 
-import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
+import { useTheme } from 'next-themes';
+import { useEffect, useLayoutEffect, useState } from 'react';
 
 export function ThemeToggle() {
   const { setTheme, theme } = useTheme();
   const [mounted, setMounted] = useState(false);
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     setMounted(true);
   }, []);
 
   if (!mounted) {
-    return null;
+    return <div className="cursor-pointer" />;
   }
 
   return (
     <div
       className="cursor-pointer"
       onClick={() => {
-        setTheme(theme === "dark" ? "light" : "dark");
+        setTheme(theme === 'dark' ? 'light' : 'dark');
       }}
     >
-      {`Toggle ${theme === "light" ? "dark" : "light"} mode`}
+      {`Toggle ${theme === 'light' ? 'dark' : 'light'} mode`}
     </div>
   );
 }
---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [],
+  },
+};
+
+export default nextConfig;
