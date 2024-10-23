33d17ebc002e8203e270cd6daa23d9cb64ec0cf1
Jared Palmer <jared@jaredpalmer.com>
2024-10-23 12:21:30 -0400

    Upgrade to Next.js 15.0.1 (#454)

--- components/custom/preview-attachment.tsx
+++ components/custom/preview-attachment.tsx
@@ -12,18 +12,18 @@ export const PreviewAttachment = ({
   const { name, url, contentType } = attachment;
 
   return (
-    <div className="flex flex-col gap-2 max-w-16">
+    (<div className="flex flex-col gap-2 max-w-16">
       <div className="h-20 w-16 bg-muted rounded-md relative flex flex-col items-center justify-center">
         {contentType ? (
           contentType.startsWith("image") ? (
             // NOTE: it is recommended to use next/image for images
             // eslint-disable-next-line @next/next/no-img-element
-            <img
+            (<img
               key={url}
               src={url}
               alt={name ?? "An image attachment"}
               className="rounded-md size-full object-cover"
-            />
+            />)
           ) : (
             <div className=""></div>
           )
@@ -37,8 +37,7 @@ export const PreviewAttachment = ({
           </div>
         )}
       </div>
-
       <div className="text-xs text-zinc-500 max-w-16 truncate">{name}</div>
-    </div>
+    </div>)
   );
 };
