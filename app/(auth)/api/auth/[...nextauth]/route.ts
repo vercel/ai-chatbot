a036a1ca68d5a2c0aa324edc5f1d18e135fd679f
Jared Palmer <jared@jaredpalmer.com>
2024-10-23 07:55:20 -0700

    npx @next/codemod@latest next-async-request-api .

--- app/(auth)/api/auth/[...nextauth]/route.ts
+++ app/(auth)/api/auth/[...nextauth]/route.ts
@@ -1,1 +1,3 @@
-export { GET, POST } from "@/app/(auth)/auth";
+export { /* @next-codemod-error `GET` export is re-exported. Check if this component uses `params` or `searchParams`*/
+GET, /* @next-codemod-error `POST` export is re-exported. Check if this component uses `params` or `searchParams`*/
+POST } from "@/app/(auth)/auth";
