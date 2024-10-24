d1ec571aaa4690992fb95a10d470ab552f5966c2
shadcn <m@shadcn.com>
2024-10-24 17:28:57 +0400

    fix

--- components/custom/sidebar-user-nav.tsx
+++ components/custom/sidebar-user-nav.tsx
@@ -1,11 +1,10 @@
 'use client';
-import { ChevronDown, ChevronUp } from 'lucide-react';
+import { ChevronUp } from 'lucide-react';
 import Image from 'next/image';
 import { type User } from 'next-auth';
 import { signOut } from 'next-auth/react';
 import { useTheme } from 'next-themes';
 
-import { ThemeToggle } from '@/components/custom/theme-toggle';
 import {
   DropdownMenu,
   DropdownMenuContent,
