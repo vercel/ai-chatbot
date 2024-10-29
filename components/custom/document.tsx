2a7059df91476f5ca26f104ff2f6a2908d8eb33e
jeremyphilemon <git@jrmyphlmn.com>
2024-10-29 23:31:11 +0530

    Tweak styles

--- components/custom/document.tsx
+++ components/custom/document.tsx
@@ -88,7 +88,7 @@ interface DocumentToolCallProps {
 
 export function DocumentToolCall({ type, args }: DocumentToolCallProps) {
   return (
-    <div className="w-fit border p-3 rounded-xl flex flex-row items-start justify-between gap-3">
+    <div className="w-fit border py-2 px-3 rounded-xl flex flex-row items-start justify-between gap-3">
       <div className="flex flex-row gap-3 items-start">
         <div className="text-zinc-500 mt-1">
           {type === 'create' ? (
