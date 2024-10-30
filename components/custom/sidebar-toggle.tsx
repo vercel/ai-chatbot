d281ed200f42c7bbc844cd4128ab72f3f7e59c3f
jeremyphilemon <git@jrmyphlmn.com>
2024-10-31 04:34:35 +0530

    Use geist icons and group history by ranges

--- components/custom/sidebar-toggle.tsx
+++ components/custom/sidebar-toggle.tsx
@@ -1,20 +1,26 @@
 import { ComponentProps } from 'react';
 
-import { SidebarTrigger } from '@/components/ui/sidebar';
+import { SidebarTrigger, useSidebar } from '@/components/ui/sidebar';
 import { BetterTooltip } from '@/components/ui/tooltip';
 import { cn } from '@/lib/utils';
+
+import { SidebarLeftIcon } from './icons';
+import { Button } from '../ui/button';
 
 export function SidebarToggle({
   className,
 }: ComponentProps<typeof SidebarTrigger>) {
+  const { toggleSidebar } = useSidebar();
+
   return (
     <BetterTooltip content="Toggle Sidebar" align="start">
-      <SidebarTrigger
-        className={cn(
-          'size-10 md:size-8 [&>svg]:!size-5 md:[&>svg]:!size-4',
-          className
-        )}
-      />
+      <Button
+        onClick={toggleSidebar}
+        variant="ghost"
+        className="md:px-2 md:h-fit"
+      >
+        <SidebarLeftIcon />
+      </Button>
     </BetterTooltip>
   );
 }
