505caa9aff8d2911dd67232fc1b7a48f9f8a20c6
Jeremy <git@jrmyphlmn.com>
2024-10-31 21:02:05 +0530

    Persist input text in local storage (#472)

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    ppr: true,
+  },
   images: {
     remotePatterns: [
       {
