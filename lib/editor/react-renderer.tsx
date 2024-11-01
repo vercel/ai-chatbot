ae1cf2e561044dc80160d28530fc0eb4aeea22fc
Jeremy <git@jrmyphlmn.com>
2024-11-01 14:13:17 +0530

    Show diff by sentence (#477)

--- lib/editor/react-renderer.tsx
+++ lib/editor/react-renderer.tsx
@@ -1,9 +1,10 @@
-import { createRoot } from "react-dom/client";
+import { createRoot } from 'react-dom/client';
 
 export class ReactRenderer {
   static render(component: React.ReactElement, dom: HTMLElement) {
     const root = createRoot(dom);
     root.render(component);
+
     return {
       destroy: () => root.unmount(),
     };
