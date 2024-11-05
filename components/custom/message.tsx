368a98658690dcaf8399cd6fb5d3d6256f5130e2
jeremyphilemon <git@jrmyphlmn.com>
2024-11-05 20:46:26 +0300

    Push route state on generation finish

--- components/custom/message.tsx
+++ components/custom/message.tsx
@@ -2,7 +2,6 @@
 
 import { Message } from 'ai';
 import cx from 'classnames';
-import { motion } from 'framer-motion';
 import { Sparkles } from 'lucide-react';
 import { Dispatch, SetStateAction } from 'react';
 
@@ -31,10 +30,8 @@ export const PreviewMessage = ({
   isLoading: boolean;
 }) => {
   return (
-    <motion.div
+    <div
       className="w-full mx-auto max-w-3xl px-4 group/message "
-      initial={{ y: 5, opacity: 0 }}
-      animate={{ y: 0, opacity: 1 }}
       data-role={message.role}
     >
       <div
@@ -145,6 +142,6 @@ export const PreviewMessage = ({
           />
         </div>
       </div>
-    </motion.div>
+    </div>
   );
 };
