cd4154c7590c747be32216e57edff10c96057d17
Yusuf Mansur Özer <ymansurozer@gmail.com>
2024-11-12 12:34:39 +0300

    improve: better model messages

--- ai/models.ts
+++ ai/models.ts
@@ -9,22 +9,22 @@ export interface Model {
 
 export const models: Array<Model> = [
   {
-    id: 'gpt-4o',
-    label: 'GPT 4o',
-    apiIdentifier: 'gpt-4o',
-    description: 'From OpenAI',
-  },
-  {
     id: 'claude-3-5-sonnet-latest',
     label: 'Claude 3.5 Sonnet',
     apiIdentifier: 'claude-3-5-sonnet-latest',
-    description: 'From Anthropic',
+    description: 'From Anthropic. Best for most tasks.',
+  },
+  {
+    id: 'gpt-4o',
+    label: 'GPT 4o',
+    apiIdentifier: 'gpt-4o',
+    description: 'From OpenAI. Good for most tasks.',
   },
   {
     id: 'llama-3.1-sonar-huge-128k-online',
     label: 'Perplexity LLama Online',
     apiIdentifier: 'llama-3.1-sonar-huge-128k-online',
-    description: 'From Perplexity, has search capabilities',
+    description: 'From Perplexity. Has access to the web.',
   },
 ] as const;
 
