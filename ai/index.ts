b6abc418c8540246ddf5a322dcfd01db8355309d
Yusuf Mansur Özer <ymansurozer@gmail.com>
2024-11-12 12:24:58 +0300

    feat: add support for anthropic & perplexity models

--- ai/index.ts
+++ ai/index.ts
@@ -1,11 +1,27 @@
-import { openai } from '@ai-sdk/openai';
-import { experimental_wrapLanguageModel as wrapLanguageModel } from 'ai';
+import { openai, createOpenAI } from '@ai-sdk/openai';
+import { anthropic } from '@ai-sdk/anthropic';
+import { LanguageModelV1, experimental_wrapLanguageModel as wrapLanguageModel } from 'ai';
 
 import { customMiddleware } from './custom-middleware';
 
 export const customModel = (apiIdentifier: string) => {
+  let model: LanguageModelV1;
+  
+  if (apiIdentifier.includes('claude')) {
+    model = anthropic(apiIdentifier);
+  } else if (apiIdentifier.includes('llama')) {
+    const perplexity = createOpenAI({
+      name: 'perplexity',
+      apiKey: process.env.PERPLEXITY_API_KEY,
+      baseURL: 'https://api.perplexity.ai/',
+    });
+    model = perplexity(apiIdentifier);
+  } else {
+    model = openai(apiIdentifier);
+  }
+
   return wrapLanguageModel({
-    model: openai(apiIdentifier),
+    model,
     middleware: customMiddleware,
   });
 };
--- ai/models.ts
+++ ai/models.ts
@@ -9,17 +9,23 @@ export interface Model {
 
 export const models: Array<Model> = [
   {
-    id: 'gpt-4o-mini',
-    label: 'GPT 4o mini',
-    apiIdentifier: 'gpt-4o-mini',
-    description: 'Small model for fast, lightweight tasks',
-  },
-  {
     id: 'gpt-4o',
     label: 'GPT 4o',
     apiIdentifier: 'gpt-4o',
-    description: 'For complex, multi-step tasks',
+    description: 'From OpenAI',
+  },
+  {
+    id: 'claude-3-5-sonnet-latest',
+    label: 'Claude 3.5 Sonnet',
+    apiIdentifier: 'claude-3-5-sonnet-latest',
+    description: 'From Anthropic',
+  },
+  {
+    id: 'llama-3.1-sonar-huge-128k-online',
+    label: 'Perplexity LLama Online',
+    apiIdentifier: 'llama-3.1-sonar-huge-128k-online',
+    description: 'From Perplexity, has search capabilities',
   },
 ] as const;
 
-export const DEFAULT_MODEL_NAME: string = 'gpt-4o-mini';
+export const DEFAULT_MODEL_NAME: string = 'claude-3-5-sonnet-latest';
