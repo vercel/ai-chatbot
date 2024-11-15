0d5f6293d16b3016a9d5ce3f6c3e33846f6afebf
Jared Palmer <jared@jaredpalmer.com>
2024-11-15 10:37:01 -0500

    Switch to biome for fomatting

--- app/(auth)/actions.ts
+++ app/(auth)/actions.ts
@@ -6,6 +6,8 @@ import { createUser, getUser } from '@/lib/db/queries';
 
 import { signIn } from './auth';
 
+
+
 const authFormSchema = z.object({
   email: z.string().email(),
   password: z.string().min(6),
@@ -61,7 +63,7 @@ export const register = async (
       password: formData.get('password'),
     });
 
-    let [user] = await getUser(validatedData.email);
+    const [user] = await getUser(validatedData.email);
 
     if (user) {
       return { status: 'user_exists' } as RegisterActionState;
