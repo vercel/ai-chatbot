e2a1f9a398989542a20a73e1ef63d8a5fde9714d
Jared Palmer <jared@jaredpalmer.com>
2024-11-15 10:15:49 -0500

    Switch to gpt-4o-mini

--- app/(chat)/actions.ts
+++ app/(chat)/actions.ts
@@ -16,7 +16,7 @@ export async function generateTitleFromUserMessage({
   message: CoreUserMessage;
 }) {
   const { text: title } = await generateText({
-    model: customModel('gpt-3.5-turbo'),
+    model: customModel('gpt-4o-mini'),
     system: `\n
     - you will generate a short title based on the first message a user begins a conversation with
     - ensure it is not more than 80 characters long
