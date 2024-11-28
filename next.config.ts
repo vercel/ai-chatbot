ccc58c1fba3721b1380fc98525e48f984426a2bb
Brianleach11 <81440466+Brianleach11@users.noreply.github.com>
2024-11-28 02:52:44 -0500

    Removed unnecessary packages, updated next config per HuggingFace docs

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    // See https://webpack.js.org/configuration/resolve/#resolvealias
+    config.resolve.alias = {
+        ...config.resolve.alias,
+        "sharp$": false,
+        "onnxruntime-node$": false,
+    }
+    return config;
+},
   experimental: {
     ppr: true,
   },
