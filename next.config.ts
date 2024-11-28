863f2013d3c6f7a42d8e15e7accf292ff2ad9b29
Brianleach11 <81440466+Brianleach11@users.noreply.github.com>
2024-11-28 04:09:47 -0500

    folded

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,9 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone', // Optional: for standalone deployment
+  serverExternalPackages: ['sharp', 'onnxruntime-node'],
   experimental: {
     ppr: true,
-    serverComponentsExternalPackages: ['sharp', 'onnxruntime-node'],
   },
   images: {
     remotePatterns: [
