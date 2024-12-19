2d8f35663999bfa58f44ad4524bd768d7744f8d7
Athos Georgiou <athrael.soju@gmail.com>
2024-12-19 18:58:08 +0200

    fix: correct type definition in useScrollToBottom hook

--- components/use-scroll-to-bottom.ts
+++ components/use-scroll-to-bottom.ts
@@ -2,7 +2,7 @@ import { useEffect, useRef, type RefObject } from 'react';
 
 export function useScrollToBottom<T extends HTMLElement>(): [
   RefObject<T>,
-  RefObject<T>,
+  RefObject<T>
 ] {
   const containerRef = useRef<T>(null);
   const endRef = useRef<T>(null);
