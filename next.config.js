12493c669324b6603d1f1083fe06947f18036e69
Bailey Simrell <baileysimrell@gmail.com>
2024-12-20 20:20:26 -0500

    Add Matplotlib support

---
+++ next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatar.vercel.sh',
+        pathname: '/**',
+      }
+    ],
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    domains: ['avatar.vercel.sh'],
+    formats: ['image/avif', 'image/webp'],
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig 
