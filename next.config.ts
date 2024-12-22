62ccf087b6d4d1f27ea5e377b81fd87ca82ecf81
Bailey Simrell <baileysimrell@gmail.com>
2024-12-22 14:18:22 -0500

    Merge next config files

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,22 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   experimental: {
     ppr: true,
   },
   images: {
     remotePatterns: [
       {
+        protocol: 'https',
         hostname: 'avatar.vercel.sh',
-      },
+        pathname: '/**',
+      }
     ],
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    formats: ['image/avif', 'image/webp'],
+    unoptimized: true,
   },
 };
 
