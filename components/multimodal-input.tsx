6f49101ea271abf4921ebfa3ca3d0252fee476a3
kavinraj A S <68454569+kavinask007@users.noreply.github.com>
2024-12-31 20:25:47 +0530

    fixed the bug where the input textbox is stuck at maxheight  for long prompts even after removing its content

--- components/multimodal-input.tsx
+++ components/multimodal-input.tsx
@@ -72,7 +72,7 @@ function PureMultimodalInput({
     if (textareaRef.current) {
       adjustHeight();
     }
-  }, []);
+  }, [input, isLoading]);
 
   const adjustHeight = () => {
     if (textareaRef.current) {
