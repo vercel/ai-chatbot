cfd0b7d09b35ee4f9af013d469731f4859d9fd0d
Avinash Mane <avinash@Dhanrajs-MacBook-Pro.local>
2025-01-04 09:08:34 +0530

    final comit

--- components/DrivePicker.jsx
+++ components/DrivePicker.jsx
@@ -1,8 +1,10 @@
 import  { useEffect } from 'react';
 import useDrivePicker from 'react-google-drive-picker'
+import {
+  DropdownMenuItem,
+} from '@/components/ui/dropdown-menu';
 
-
-function App() {
+function App({ setUploadQueue, setAttachments, uploadFile, setOpen}) {
   const [openPicker, data, authResponse] = useDrivePicker();  
   // const customViewsArray = [new google.picker.DocsView()]; // custom view
   const handleOpenPicker = () => {
@@ -19,25 +21,47 @@ function App() {
       callbackFunction: (data) => {
         if (data.action === 'cancel') {
           console.log('User clicked cancel/close button')
+        }else if(data.action == "picked"){
+          renderDriveFiles(data.docs)
         }
+        console.log(data)
         console.log(data)
       },
     })
   }
 
-  useEffect(()=>{
-    if(data){
-        data.docs.map(i=>console.log(i))
+  const renderDriveFiles = async (files) =>{
+      setUploadQueue(files.map((file) => file.name));
+      try {
+        setAttachments((currentAttachments) => [
+          ...currentAttachments,
+          ...files.map(file=>({ ...file, contentType: file.mimeType, url: file.embedUrl})),
+        ]);
+      } catch (error) {
+        console.error('Error uploading files!', error);
+      } finally {
+        setUploadQueue([]);
+      }
     }
 
-  }, [data])
-
+  
 
   
   return (
-    <div>
-        <button onClick={() => handleOpenPicker()}>Open Picker</button>
-    </div>
+    <DropdownMenuItem
+       onSelect={() => {
+         setOpen(false);
+       }}
+       onClick={() => handleOpenPicker()}
+       className="gap-2 group/item flex flex-row justify-between items-center"
+     >
+       <div className="flex flex-col gap-1 items-start">
+         Connect to google drive
+       </div>
+       <div className="text-foreground dark:text-foreground group-data-[active=true]/item:opacity-100">
+         <img src='/images/drive.png' className='h-4 bg-slate-200'/>
+       </div>
+     </DropdownMenuItem>
   );
 }
 
