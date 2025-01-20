6b0f6277d249b3f03e9395c2d7956722fd709846
Athrael Soju <athrael.soju@gmail.com>
2025-01-20 16:06:00 +0000

    feat: add KaTeX support and change rehypeKatex output format to 'html'

--- components/markdown.tsx
+++ components/markdown.tsx
@@ -1,3 +1,4 @@
+import 'katex/dist/katex.min.css';
 import Link from 'next/link';
 import React, { memo, useMemo, useState } from 'react';
 import ReactMarkdown, { type Components } from 'react-markdown';
@@ -106,7 +107,7 @@ const NonMemoizedMarkdown = ({ children }: { children: string }) => {
   return (
     <ReactMarkdown
       remarkPlugins={[remarkGfm, [remarkMath, { singleDollarTextMath: false }]]}
-      rehypePlugins={[[rehypeKatex, { output: 'markdown' }]]}
+      rehypePlugins={[[rehypeKatex, { output: 'html' }]]}
       components={components}
     >
       {processedText}
