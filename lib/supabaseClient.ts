336765e6d83df45621284e02a4eb165821070570
Nishant Bundela <nishantbundela@gmail.com>
2025-02-24 14:23:52 -0500

    removed old supabase files

--- lib/supabaseClient.ts
+++
@@ -1,8 +0,0 @@
-// lib/supabaseClient.ts
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-
-// This client can be used in client-side components
-export const supabaseClient = createClient(supabaseUrl, supabaseAnonKey);
--- lib/supabaseServer.ts
+++
@@ -1,7 +0,0 @@
-// lib/supabaseServer.ts
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
-
-export const supabaseServer = () => {
-  return createServerComponentClient({ cookies });
-};
