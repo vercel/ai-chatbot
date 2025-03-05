bdd1b1cc6da04f97fc4ad61de17c074c38b1c426
Jeremy <git@jrmyphlmn.com>
2025-03-04 18:00:14 -0800

    chore: bump versions (#845)

--- components/data-stream-handler.tsx
+++ components/data-stream-handler.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useChat } from 'ai/react';
+import { useChat } from '@ai-sdk/react';
 import { useEffect, useRef } from 'react';
 import { artifactDefinitions, ArtifactKind } from './artifact';
 import { Suggestion } from '@/lib/db/schema';
