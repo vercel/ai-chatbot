b42c46cbe747f5410701f6de8043259116fe4bcf
Ved Gupta <vedgupta@protonmail.com>
2025-03-22 00:46:17 +0530

    gemini added

--- lib/ai/models.ts
+++ lib/ai/models.ts
@@ -1,4 +1,4 @@
-export const DEFAULT_CHAT_MODEL: string = 'chat-llama3-70b';
+export const DEFAULT_CHAT_MODEL: string = 'chat-gemini-2.0-flash-lite';
 
 interface ChatModel {
   id: string;
@@ -18,9 +18,21 @@ export const chatModels: Array<ChatModel> = [
     description: 'For complex, multi-step tasks with versatility',
   },
   {
-    id: 'chat-llama3.3-70b-specdec',
-    name: 'Llama 3.3 70b SpecDec',
-    description: 'For complex, multi-step tasks with specialized decoding',
+    id: 'chat-gemini-2.0-flash-lite',
+    name: 'Gemini 2.0 Flash-Lite',
+    description: 'LLM model, built by Google',
+  },
+  {
+    id: 'chat-gemini-1.5-flash-8b',
+    name: 'Gemini 1.5 Flash-8B',
+    description:
+      'Built by Google, It is a small model designed for lower intelligence tasks.',
+  },
+  {
+    id: 'chat-gemini-2.0-flash',
+    name: 'Gemini 2.0 Flash',
+    description:
+      'Built by Google, Next generation features, speed, thinking, realtime streaming',
   },
   {
     id: 'chat-qwen2.5-32b',
--- lib/ai/providers.ts
+++ lib/ai/providers.ts
@@ -3,7 +3,10 @@ import {
   extractReasoningMiddleware,
   wrapLanguageModel,
 } from 'ai';
+
 import { groq } from '@ai-sdk/groq';
+import { google } from '@ai-sdk/google';
+
 import { isTestEnvironment } from '../constants';
 import {
   artifactModel,
@@ -25,8 +28,10 @@ export const myProvider = isTestEnvironment
       languageModels: {
         'chat-llama3-70b': groq('llama3-70b-8192'),
         'chat-llama3.3-70b-versatile': groq('llama-3.3-70b-versatile'),
-        'chat-llama3.3-70b-specdec': groq('llama-3.3-70b-specdec'),
         'chat-qwen2.5-32b': groq('qwen-2.5-32b'),
+        'chat-gemini-2.0-flash-lite': google('gemini-2.0-flash-lite'),
+        'chat-gemini-1.5-flash-8b': google('gemini-1.5-flash-8b'),
+        'chat-gemini-2.0-flash': google('gemini-2.0-flash'),
         'chat-model-reasoning': wrapLanguageModel({
           model: groq('deepseek-r1-distill-qwen-32b'),
           middleware: extractReasoningMiddleware({ tagName: 'think' }),
