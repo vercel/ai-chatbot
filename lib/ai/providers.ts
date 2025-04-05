110fc32b51a0ba8e63034ca45346badbb0aa2d90
inwesol <inwesol.coach@gmail.com>
2025-04-05 23:56:20 +0530

    added models.

--- lib/ai/providers.ts
+++ lib/ai/providers.ts
@@ -5,6 +5,7 @@ import {
 } from 'ai';
 import { groq } from '@ai-sdk/groq';
 import { xai } from '@ai-sdk/xai';
+import { openai } from '@ai-sdk/openai';
 import { isTestEnvironment } from '../constants';
 import {
   artifactModel,
@@ -24,13 +25,16 @@ export const myProvider = isTestEnvironment
     })
   : customProvider({
       languageModels: {
-        'chat-model': xai('grok-2-1212'),
+      // 'chat-model': xai('grok-2-1212'),
+        'chat-model': openai('gpt-4o-mini'),
         'chat-model-reasoning': wrapLanguageModel({
           model: groq('deepseek-r1-distill-llama-70b'),
           middleware: extractReasoningMiddleware({ tagName: 'think' }),
         }),
-        'title-model': xai('grok-2-1212'),
-        'artifact-model': xai('grok-2-1212'),
+      // 'title-model': xai('grok-2-1212'),
+        'title-model': openai('gpt-4o-mini'),
+      // 'artifact-model': xai('grok-2-1212'),
+        'artifact-model': openai('gpt-4o-mini'),
       },
       imageModels: {
         'small-model': xai.image('grok-2-image'),
--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  devIndicators: false,
   experimental: {
     ppr: true,
   },
