6ca8a3f29ac108343bdac8bdb053291d709dadae
jeremyphilemon <git@jrmyphlmn.com>
2025-04-07 10:34:53 -0700

    feat: replace overview with greeting

---
+++ components/greeting.tsx
@@ -0,0 +1,29 @@
+import { motion } from 'framer-motion';
+
+export const Greeting = () => {
+  return (
+    <div
+      key="overview"
+      className="max-w-3xl mx-auto md:mt-20 px-8 size-full flex flex-col justify-center"
+    >
+      <motion.div
+        initial={{ opacity: 0, y: 10 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 10 }}
+        transition={{ delay: 0.5 }}
+        className="text-2xl font-semibold"
+      >
+        Hello there!
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, y: 10 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 10 }}
+        transition={{ delay: 0.6 }}
+        className="text-2xl text-zinc-500"
+      >
+        How can I help you today?
+      </motion.div>
+    </div>
+  );
+};
--- components/messages.tsx
+++ components/messages.tsx
@@ -1,12 +1,12 @@
-import { ChatRequestOptions, UIMessage } from 'ai';
+import type { ChatRequestOptions, UIMessage } from 'ai';
 import { PreviewMessage, ThinkingMessage } from './message';
 import { useScrollToBottom } from './use-scroll-to-bottom';
-import { Overview } from './overview';
 import { memo } from 'react';
-import { Vote } from '@/lib/db/schema';
+import type { Vote } from '@/lib/db/schema';
 import equal from 'fast-deep-equal';
-import { User } from 'next-auth';
-import { UseChatHelpers } from '@ai-sdk/react';
+import type { User } from 'next-auth';
+import type { UseChatHelpers } from '@ai-sdk/react';
+import { Greeting } from './greeting';
 
 interface MessagesProps {
   chatId: string;
@@ -40,7 +40,7 @@ function PureMessages({
       ref={messagesContainerRef}
       className="flex flex-col min-w-0 gap-6 flex-1 overflow-y-scroll pt-4"
     >
-      {messages.length === 0 && <Overview />}
+      {messages.length === 0 && <Greeting />}
 
       {messages.map((message, index) => (
         <PreviewMessage
