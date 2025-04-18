a9dd079d28d99ec74de769496622631c21736af4
chriswinsatlife <1500666+chriswinsatlife@users.noreply.github.com>
2025-04-17 23:38:34 -0400

    Update providers.ts

--- lib/ai/providers.ts
+++ lib/ai/providers.ts
@@ -2,44 +2,44 @@ import {
   customProvider,
   extractReasoningMiddleware,
   wrapLanguageModel,
-} from "ai";
-import { groq } from "@ai-sdk/groq";
-import { xai } from "@ai-sdk/xai";
-import { google } from "@ai-sdk/google";
-import { fal } from "@ai-sdk/fal";
-import { togetherai } from "@ai-sdk/togetherai";
-import { openai, OpenAIResponsesProviderOptions } from "@ai-sdk/openai";
-import { anthropic } from "@ai-sdk/anthropic";
-import { fireworks } from "@ai-sdk/fireworks";
-import { replicate } from "@ai-sdk/replicate";
-import { isTestEnvironment } from "../constants";
+} from 'ai';
+import { groq } from '@ai-sdk/groq';
+import { xai } from '@ai-sdk/xai';
+import { google } from '@ai-sdk/google';
+import { fal } from '@ai-sdk/fal';
+import { togetherai } from '@ai-sdk/togetherai';
+import { openai, OpenAIResponsesProviderOptions } from '@ai-sdk/openai';
+import { anthropic } from '@ai-sdk/anthropic';
+import { fireworks } from '@ai-sdk/fireworks';
+import { replicate } from '@ai-sdk/replicate';
+import { isTestEnvironment } from '../constants';
 import {
   artifactModel,
   chatModel,
   reasoningModel,
   titleModel,
-} from "./models.test";
+} from './models.test';
 
 export const myProvider = isTestEnvironment
   ? customProvider({
-    languageModels: {
-      "chat-model": chatModel,
-      "chat-model-reasoning": reasoningModel,
-      "title-model": titleModel,
-      "artifact-model": artifactModel,
-    },
-  })
+      languageModels: {
+        'chat-model': chatModel,
+        'chat-model-reasoning': reasoningModel,
+        'title-model': titleModel,
+        'artifact-model': artifactModel,
+      },
+    })
   : customProvider({
-    languageModels: {
-      "chat-model": openai("gpt-4.1"),
-      "chat-model-reasoning": wrapLanguageModel({
-        model: openai("o3"),
-        middleware: extractReasoningMiddleware({ tagName: "think" }),
-      }),
-      "title-model": openai("gpt-4.1-mini"),
-      "artifact-model": google("gemini-2.5-pro-exp-03-25"),
-    },
-    imageModels: {
-      "small-model": fal.image("fal-ai/recraft-v3"),
-    },
-  });
+      languageModels: {
+        'chat-model': openai('gpt-4.1'),
+        'chat-model-reasoning': wrapLanguageModel({
+          model: openai('o3'),
+          middleware: extractReasoningMiddleware({ tagName: 'think' }),
+        }),
+        'title-model': openai('gpt-4.1-mini'),
+        'artifact-model': google('gemini-2.5-pro-exp-03-25'),
+      },
+      imageModels: {
+        'small-model': fal.image('fal-ai/recraft-v3'),
+      },
+    });
