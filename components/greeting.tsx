517753fbbbaf29d5238a3bad4704ad695d4ba1bc
Temporary User <chriswinsatlife@users.noreply.github.com>
2025-04-21 11:53:27 +0800

    fix: Escape apostrophe in Greeting component

--- components/greeting.tsx
+++ components/greeting.tsx
@@ -13,7 +13,7 @@ export const Greeting = () => {
         transition={{ delay: 0.5 }}
         className="text-2xl font-semibold"
       >
-        Let's go.
+        Let&apos;s go.
       </motion.div>
       <motion.div
         initial={{ opacity: 0, y: 10 }}
