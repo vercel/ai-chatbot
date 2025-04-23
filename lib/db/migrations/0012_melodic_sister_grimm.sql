b7d12eb7fb54ae56d1128998cc17363464a3a619
Temporary User <chriswinsatlife@users.noreply.github.com>
2025-04-23 18:04:22 +0800

    delete stupid ai file

--- lib/db/migrations/0012_melodic_sister_grimm.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE "Message";--> statement-breakpoint
-DROP TABLE "Vote";--> statement-breakpoint
-ALTER TABLE "Chat" DROP CONSTRAINT "Chat_userId_User_Profiles_id_fk";
---> statement-breakpoint
-DO $$ BEGIN
- ALTER TABLE "Chat" ADD CONSTRAINT "Chat_userId_User_Profiles_id_fk" FOREIGN KEY ("userId") REFERENCES "public"."User_Profiles"("id") ON DELETE cascade ON UPDATE no action;
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
