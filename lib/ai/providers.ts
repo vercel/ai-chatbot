6375b0128a53ee1cd40ee668bf0473374cef4e04
inwesol <inwesol.coach@gmail.com>
2025-05-09 17:12:40 +0530

    prompts updated and provider changed to gpt-4o.

--- lib/ai/providers.ts
+++ lib/ai/providers.ts
@@ -25,15 +25,16 @@ export const myProvider = isTestEnvironment
     })
   : customProvider({
       languageModels: {
-      // 'chat-model': xai('grok-2-1212'),
-        'chat-model': openai('gpt-4o-mini'),
+        // 'chat-model': xai('grok-2-1212'),
+        // 'chat-model': openai('gpt-4o-mini'),
+        'chat-model': openai('gpt-4o'),
         'chat-model-reasoning': wrapLanguageModel({
           model: groq('deepseek-r1-distill-llama-70b'),
           middleware: extractReasoningMiddleware({ tagName: 'think' }),
         }),
-      // 'title-model': xai('grok-2-1212'),
+        // 'title-model': xai('grok-2-1212'),
         'title-model': openai('gpt-4o-mini'),
-      // 'artifact-model': xai('grok-2-1212'),
+        // 'artifact-model': xai('grok-2-1212'),
         'artifact-model': openai('gpt-4o-mini'),
       },
       imageModels: {
