85dd8941023d580d270d5b01c0d3764a432fe564
Jimmy Moon <ragingwind@gmail.com>
2025-05-13 20:26:30 +0900

    fix: fix code block error

--- components/code-block.tsx
+++ components/code-block.tsx
@@ -16,7 +16,7 @@ export function CodeBlock({
 }: CodeBlockProps) {
   if (!inline) {
     return (
-      <div className="not-prose flex flex-col">
+      <div className="not-prose flex flex-col display-block">
         <pre
           {...props}
           className={`text-sm w-full overflow-x-auto dark:bg-zinc-900 p-4 border border-zinc-200 dark:border-zinc-700 rounded-xl dark:text-zinc-50 text-zinc-900`}
