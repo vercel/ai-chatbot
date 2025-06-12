5d2af31b90b935a85d56fae0fc333d451577a4ca
Denis Kiselev <deksden@deksden.com>
2025-06-12 14:43:26 +0200

    feat: vercel's speedinsight support

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Toaster } from 'sonner';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import { ThemeProvider } from '@/components/theme-provider';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import './globals.css';
 import { SessionProvider } from 'next-auth/react';
@@ -71,6 +72,7 @@ export default async function RootLayout({
         />
       </head>
       <body className="antialiased">
+        <SpeedInsights/>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
