5cb4f8f62f60ca86c5c697e4c268d0a6d84269b7
NovahEra <666meju@gmail.com>
2025-07-29 00:11:56 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,35 +2,29 @@ import { Toaster } from 'sonner';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import { ThemeProvider } from '@/components/theme-provider';
-
 import './globals.css';
 import { SessionProvider } from 'next-auth/react';
-
 export const metadata: Metadata = {
   metadataBase: new URL('https://chat.vercel.ai'),
   title: 'Next.js Chatbot Template',
   description: 'Next.js chatbot template using the AI SDK.',
 };
-
 export const viewport = {
   maximumScale: 1, // Disable auto-zoom on mobile Safari
 };
-
 const geist = Geist({
   subsets: ['latin'],
   display: 'swap',
   variable: '--font-geist',
 });
-
 const geistMono = Geist_Mono({
   subsets: ['latin'],
   display: 'swap',
   variable: '--font-geist-mono',
 });
-
 const LIGHT_THEME_COLOR = 'hsl(0 0% 100%)';
 const DARK_THEME_COLOR = 'hsl(240deg 10% 3.92%)';
-const THEME_COLOR_SCRIPT = `\
+const THEME_COLOR_SCRIPT = \
 (function() {
   var html = document.documentElement;
   var meta = document.querySelector('meta[name="theme-color"]');
@@ -46,8 +40,7 @@ const THEME_COLOR_SCRIPT = `\
   var observer = new MutationObserver(updateThemeColor);
   observer.observe(html, { attributes: true, attributeFilter: ['class'] });
   updateThemeColor();
-})();`;
-
+})();;
 export default async function RootLayout({
   children,
 }: Readonly<{
@@ -56,12 +49,12 @@ export default async function RootLayout({
   return (
     <html
       lang="en"
-      // `next-themes` injects an extra classname to the body element to avoid
-      // visual flicker before hydration. Hence the `suppressHydrationWarning`
+      // next-themes injects an extra classname to the body element to avoid
+      // visual flicker before hydration. Hence the suppressHydrationWarning
       // prop is necessary to avoid the React hydration mismatch warning.
       // https://github.com/pacocoursey/next-themes?tab=readme-ov-file#with-app
       suppressHydrationWarning
-      className={`${geist.variable} ${geistMono.variable}`}
+      className={${geist.variable} ${geistMono.variable}}
     >
       <head>
         <script
