359738b001cb2fdc403873a2f5f62050092ec655
raky291 <raky291@gmail.com>
2025-08-24 14:34:49 -0700

    Fix textarea height

--- components/multimodal-input.tsx
+++ components/multimodal-input.tsx
@@ -283,7 +283,7 @@ function PureMultimodalInput({
         value={input}
         onChange={handleInput}
         className={cx(
-          'min-h-[24px] max-h-[calc(75dvh)] overflow-hidden resize-none rounded-2xl text-base! bg-muted pb-10 dark:border-zinc-700',
+          'min-h-[24px] max-h-[calc(75dvh)] field-sizing-fixed overflow-hidden resize-none rounded-2xl text-base! bg-muted pb-10 dark:border-zinc-700',
           className,
         )}
         rows={2}
