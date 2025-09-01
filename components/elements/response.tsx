5e2f45aede82cb427d16d6f62c521f0bc6798fb3
josh <144584931+dancer@users.noreply.github.com>
2025-09-01 11:58:16 +0100

    fix: split layout (#1147)

--- components/elements/response.tsx
+++ components/elements/response.tsx
@@ -10,7 +10,7 @@ export const Response = memo(
   ({ className, ...props }: ResponseProps) => (
     <Streamdown
       className={cn(
-        'size-full [&>*:first-child]:mt-0 [&>*:last-child]:mb-0',
+        'size-full [&>*:first-child]:mt-0 [&>*:last-child]:mb-0 [&_pre]:overflow-x-auto [&_pre]:max-w-full [&_code]:break-words [&_code]:whitespace-pre-wrap',
         className,
       )}
       {...props}
