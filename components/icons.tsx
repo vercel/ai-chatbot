2f9ba0960686035b5a344d6f98fe13a9224dafd3
josh <144584931+dancer@users.noreply.github.com>
2025-09-09 23:58:40 +0100

    ui: changes for context menu plus alignment + padding (#1179)

--- components/icons.tsx
+++ components/icons.tsx
@@ -698,6 +698,30 @@ export const DeltaIcon = ({ size = 16 }: { size?: number }) => (
   </svg>
 );
 
+export const CpuIcon = ({ size = 16 }: { size?: number }) => (
+  <svg
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    style={{ color: 'currentcolor' }}
+  >
+    <path d="M4 12C4 8.22876 4 6.34315 5.17157 5.17157C6.34315 4 8.22876 4 12 4C15.7712 4 17.6569 4 18.8284 5.17157C20 6.34315 20 8.22876 20 12C20 15.7712 20 17.6569 18.8284 18.8284C17.6569 20 15.7712 20 12 20C8.22876 20 6.34315 20 5.17157 18.8284C4 17.6569 4 15.7712 4 12Z" strokeLinejoin="round" />
+    <path d="M9.5 2V4" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M14.5 2V4" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M9.5 20V22" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M14.5 20V22" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M13 9L9 13" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M15 13L13 15" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M22 14.5L20 14.5" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M4 9.5L2 9.5" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M4 14.5L2 14.5" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M22 9.5L20 9.5" strokeLinecap="round" strokeLinejoin="round" />
+  </svg>
+);
+
 export const PenIcon = ({ size = 16 }: { size?: number }) => (
   <svg
     height={size}
