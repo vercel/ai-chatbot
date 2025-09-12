0cb15cc9fee8a0eda8edc507810d06bb013d5b31
own-boldsbrain <fernando@boldsbrain.ai>
2025-09-12 19:50:33 -0300

    feat: add Sandpack integration

---
+++ components/sandpack.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import '@codesandbox/sandpack-react/dist/index.css';
+import { Sandpack } from '@codesandbox/sandpack-react';
+
+export function SandpackPreview() {
+  return <Sandpack template="react" />;
+}
