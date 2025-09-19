ffe662ca4de8a7136ccae3f2784fd415ab34c98c
josh <josh@afterima.ge>
2025-09-19 15:38:22 +0100

    chore: rounded xl on suggested actions + user messages

--- components/message.tsx
+++ components/message.tsx
@@ -129,7 +129,7 @@ const PurePreviewMessage = ({
                     <MessageContent
                       data-testid="message-content"
                       className={cn({
-                        'w-fit break-words rounded-2xl px-3 py-2 text-right text-white':
+                        'w-fit break-words rounded-xl px-3 py-2 text-right text-white':
                           message.role === 'user',
                         'bg-transparent px-0 py-0 text-left':
                           message.role === 'assistant',
