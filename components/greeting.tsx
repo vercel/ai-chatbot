176cab910e483bbd4f9d0529c9e14f13e602efcd
Hassan Hammam <hammamhassan562@gmail.com>
2025-10-29 13:51:59 +0300

    Update greeting.tsx

--- components/greeting.tsx
+++ components/greeting.tsx
@@ -7,7 +7,7 @@ export const Greeting = () => {
       className="max-w-3xl mx-auto md:mt-20 px-8 size-full flex flex-col justify-center"
     >
       <motion.div
-        initial={{ opacity: 0, y: 10 }}
+        initial={{ opacity: 0.5, y: 10 }}
         animate={{ opacity: 1, y: 0 }}
         exit={{ opacity: 0, y: 10 }}
         transition={{ delay: 0.5 }}
