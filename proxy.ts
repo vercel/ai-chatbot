1871fa322d5d4238e01980333a722e00986cb262
Hayden Bleasel <hello@haydenbleasel.com>
2025-11-01 14:00:50 -0700

    Update proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -3,10 +3,6 @@ import { type NextRequest, NextResponse } from "next/server";
 export const proxy = (request: NextRequest) => {
   const { pathname } = request.nextUrl;
 
-  if (pathname.startsWith("/ping")) {
-    return new Response("pong", { status: 200 });
-  }
-
   if (pathname.startsWith("/api/auth")) {
     return NextResponse.next();
   }
