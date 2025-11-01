7320d766ac3dd912597708654e43fd7e477311d5
Hayden Bleasel <hello@haydenbleasel.com>
2025-11-01 15:29:15 -0700

    Use SidebarTrigger

--- components/chat-header.tsx
+++ components/chat-header.tsx
@@ -4,10 +4,9 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { memo } from "react";
 import { useWindowSize } from "usehooks-ts";
-import { SidebarToggle } from "@/components/sidebar-toggle";
 import { Button } from "@/components/ui/button";
 import { PlusIcon, VercelIcon } from "./icons";
-import { useSidebar } from "./ui/sidebar";
+import { SidebarTrigger, useSidebar } from "./ui/sidebar";
 import { VisibilitySelector, type VisibilityType } from "./visibility-selector";
 
 function PureChatHeader({
@@ -26,7 +25,7 @@ function PureChatHeader({
 
   return (
     <header className="sticky top-0 flex items-center gap-2 bg-background px-2 py-1.5 md:px-2">
-      <SidebarToggle />
+      <SidebarTrigger />
 
       {(!open || windowWidth < 768) && (
         <Button
