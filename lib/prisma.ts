67513a1f7b8ec0c1c91da2943af7deae07b7f677
josh <josh@afterima.ge>
2025-11-01 21:16:27 +0000

    prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@/generated/client";
 
 const globalforprisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
