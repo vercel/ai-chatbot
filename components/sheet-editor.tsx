bf92efe679e0d98d03b44c07b7a0094bb49c0348
josh <josh@afterima.ge>
2025-11-01 21:14:11 +0000

    import

--- components/sheet-editor.tsx
+++ components/sheet-editor.tsx
@@ -3,7 +3,7 @@
 import { useTheme } from "next-themes";
 import { parse, unparse } from "papaparse";
 import { memo, useEffect, useMemo, useState } from "react";
-import DataGrid, { textEditor } from "react-data-grid";
+import { DataGrid, textEditor } from "react-data-grid";
 import { cn } from "@/lib/utils";
 
 import "react-data-grid/lib/styles.css";
