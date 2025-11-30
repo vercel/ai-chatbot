394ea1e3cd6caec67d79aec001c7d973d8c36aaa
linuxui <75408174+linuxui@users.noreply.github.com>
2025-11-29 19:54:38 -0500

    Update suggested actions to include Spanish queries

--- components/suggested-actions.tsx
+++ components/suggested-actions.tsx
@@ -15,10 +15,10 @@ type SuggestedActionsProps = {
 
 function PureSuggestedActions({ chatId, sendMessage }: SuggestedActionsProps) {
   const suggestedActions = [
-    "What are the advantages of using Next.js?",
-    "Write code to demonstrate Dijkstra's algorithm",
-    "Help me write an essay about Silicon Valley",
-    "What is the weather in San Francisco?",
+    "¿Cuáles son las ventajas de usar Mi Agente X?",
+    "¿Donde puedo aplicar para una visa de tranbajo el los EEUU",
+    "Ayudame a solicitar empleo en el area donde vivo",
+    "¿Cuál es el clima en Sarasota, FL?,
   ];
 
   return (
