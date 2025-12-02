e6583c53e9e997232bb12a1824abf117ace611b7
Aivin V. Solatorio <avsolatorio@gmail.com>
2025-12-01 19:47:39 -0500

    Remove unused imports
    Signed-off-by: Aivin V. Solatorio <avsolatorio@gmail.com>

--- components/chat-header.tsx
+++ components/chat-header.tsx
@@ -1,12 +1,11 @@
 "use client";
 
-import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { memo } from "react";
 import { useWindowSize } from "usehooks-ts";
 import { SidebarToggle } from "@/components/sidebar-toggle";
 import { Button } from "@/components/ui/button";
-import { PlusIcon, VercelIcon } from "./icons";
+import { PlusIcon } from "./icons";
 import { useSidebar } from "./ui/sidebar";
 import { VisibilitySelector, type VisibilityType } from "./visibility-selector";
 
