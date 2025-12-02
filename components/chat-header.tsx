0f25fb4e5f44fb88ae30c54923717c1591e3cec8
Aivin V. Solatorio <avsolatorio@gmail.com>
2025-12-01 19:40:06 -0500

    Remove deploy button
    Signed-off-by: Aivin V. Solatorio <avsolatorio@gmail.com>

--- components/chat-header.tsx
+++ components/chat-header.tsx
@@ -49,20 +49,6 @@ function PureChatHeader({
           selectedVisibilityType={selectedVisibilityType}
         />
       )}
-
-      <Button
-        asChild
-        className="order-3 hidden bg-zinc-900 px-2 text-zinc-50 hover:bg-zinc-800 md:ml-auto md:flex md:h-fit dark:bg-zinc-100 dark:text-zinc-900 dark:hover:bg-zinc-200"
-      >
-        <Link
-          href={"https://vercel.com/templates/next.js/nextjs-ai-chatbot"}
-          rel="noreferrer"
-          target="_noblank"
-        >
-          <VercelIcon size={16} />
-          Deploy with Vercel
-        </Link>
-      </Button>
     </header>
   );
 }
