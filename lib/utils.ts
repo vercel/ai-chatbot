a8609e547af4eb85330fdd5d048bffdbbc600f9f
Fizzybrew <alexander200814@mail.ru>
2026-09-09 18:50:31 +0300

    fix(utils): use crypto.randomUUID for UUID generation

--- lib/utils.ts
+++ lib/utils.ts
@@ -47,11 +47,7 @@ export async function fetchWithErrorHandlers(
 }
 
 export function generateUUID(): string {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-    const r = (Math.random() * 16) | 0;
-    const v = c === 'x' ? r : (r & 0x3) | 0x8;
-    return v.toString(16);
-  });
+  return crypto.randomUUID();
 }
 
 export function getDocumentTimestampByIndex(
